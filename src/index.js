a7813475345bb8043d907327f870539396e0e4b9
nagisano33 <vita.stars2@gmail.com>
2023-06-14 01:36:34 +0900

    Add 未実装の関数を定義

---
+++ src/index.js
@@ -0,0 +1,16 @@
+export function loop(times, callback) {
+  // callback() を times 回 呼び出すプログラムを書くこと
+}
+
+export function nabeatsu(num) {
+  // num が 3 の倍数なら true, それ以外は false を返すこと
+}
+
+export function getScoreMessage(point) {
+    // point が取りうる値は 0 〜 100 で、範囲外の場合は
+    // throw new Error() でエラーを投げること
+    // point が 100 の場合は　'perfect'
+    // 80 以上の場合は 'nice'
+    // 60 以上の場合は 'good'
+    // 59 以下の場合は 'bad' を返すこと
+}
