6884a521fd36c5a91607dde1db7e9f560f75fbc5
GO666 <higuchi.gosuke@gmail.com>
2023-06-20 13:30:07 +0900

    function nabeatu getScoreMessageを修正

--- src/index.js
+++ src/index.js
@@ -1,18 +1,13 @@
 export function loop(times, callback) {
   // callback() を times 回 呼び出すプログラムを書くこと
-  for(let i=0;i<times;i++){ //違う気がする
+  for(let i=0;i<times;i++){ 
   callback();
 }
 }
 
 export function nabeatsu(num) {
   // num が 3 の倍数なら true, それ以外は false を返すこと
-  if(num%3===0){
-return true;
-  }
-  else{
-   return false;
-  }
+  return num%3===0;
 }
 
 export function getScoreMessage(point) {
@@ -22,41 +17,17 @@ export function getScoreMessage(point) {
     // 80 以上の場合は 'nice'
     // 60 以上の場合は 'good'
     // 59 以下の場合は 'bad' を返すこと
-
-
-    /*
-    const point = 100;
-    switch(true){
-      case point === 100://100の時
-      return "perfect";
-      break;
-
-      case 80<=point && point<100: //80~99の時
-      return "nice";
-      break;
-
-      case 60<=point && point<80: //60~79の時
-      return "good";
-      break;
-
-      case 0<=point && point<60: //0~59の時
-      return "bad";
-      break;
-      
-      default: 
-      Error(); //範囲外の時エラー
-    }*/
-
+    
     if(point===100){
       return "perfect";
-    }else if(point<0 || 100<point) {
+    }else if(point<0 || 100<point){
       throw new Error();
     }else if(80<=point){
       return "nice";
     }else if(60<=point){
       return "good";
-    }else if(0<=point){
-      return "bad";
+    }else {
+      return "bad"; 
     }
   }
 
